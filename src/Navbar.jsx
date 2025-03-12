0ad0ed2b6fb005f418d01534c232af0a1eec9929
dev-teng <vnctlcrn@gmail.com>
2025-03-12 18:36:47 +0800

    added x navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -9,7 +9,9 @@ function Navbar () {
           <div className="container-fluid ">
               <Link to="/" className="navbar-brand fw-bold text-center"><img className="img-fluid rounded rounded-circle" style={{width: "4rem"}} src={Logo} /></Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="toggle navigation">
-              <span className="navbar-toggler-icon"></span>
+              <span className="toggler-icon top-bar"></span>
+              <span className="toggler-icon middle-bar"></span>
+              <span className="toggler-icon bottom-bar"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarText">
               <ul className="navbar-nav mb-2 mb-lg-0">
