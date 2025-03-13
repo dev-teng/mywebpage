630be3a522bf229329ca5488af37aafd2950c85a
dev-teng <vnctlcrn@gmail.com>
2025-03-13 10:32:55 +0800

    fixed the hamburger x

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,27 +5,28 @@ import Logo from "/images/logo.png";
 function Navbar () {
   return(
     <div>
-      <nav className="navbar navbar-expand-lg navbar-transparent fixed-top shadow p-3">
-          <div className="container-fluid ">
-              <Link to="/" className="navbar-brand fw-bold text-center"><img className="img-fluid rounded rounded-circle" style={{width: "4rem"}} src={Logo} /></Link>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="toggle navigation">
-              <span className="toggler-icon top-bar"></span>
-              <span className="toggler-icon middle-bar"></span>
-              <span className="toggler-icon bottom-bar"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarText">
-              <ul className="navbar-nav mb-2 mb-lg-0">
-                  <li className="nav-item">
-                    <Link to="/about" className="nav-link active" >About</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link to="/project" className="nav-link active" >Project</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link to="/contact" className="nav-link active" >Contact</Link>
-                  </li>
-              </ul>
-            </div>
+      <nav class="navbar navbar-expand-lg navbar-transparent fixed-top shadow p-3">
+          <div class="container-fluid">
+              <Link to="/" class="navbar-brand"><img className="img-fluid rounded rounded-circle" style={{width: "4rem"}} src={Logo} /></Link>
+                <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="toggler-icon top-bar"></span>
+                    <span class="toggler-icon middle-bar"></span>
+                    <span class="toggler-icon bottom-bar"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                          <li class="nav-item">
+                            <Link to="/about" class="nav-link active" aria-current="page" href="#">About</Link>
+                          </li>
+                          <li class="nav-item">
+                            <Link to="/project" class="nav-link active" aria-current="page" href="#">Project</Link>
+                          </li>
+                          <li class="nav-item">
+                            <Link to="/contact" class="nav-link active" aria-current="page" href="#">Contact</Link>
+                          </li>
+                    </ul>
+                </div>
           </div>
       </nav>
         <Outlet />
