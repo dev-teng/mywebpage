d8b19882b6763db7f42d8dca10fc058e108253f8
dev-teng <vnctlcrn@gmail.com>
2025-03-13 11:18:56 +0800

    added a button download cv

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,22 +3,26 @@ import "./Layout.css";
 function Home() {
   return (
     <div className="d-flex flex-column min-vh-100 background">
-      <div className="row g-0 ">
-        <div className="col-md-12 text-center">
-          <img className='img-fluid' src={Vincent} style={{ width: '35rem' }} />
-        </div>
-        <div className="row g-0 ">
-          <div className="col-md-4"></div>
-          <div className="col-md-4 text-center">
-            <h5 className='text-white p-4 greeting rounded' >
-              <p className='welcome'>Welcome to My Corner of the Web!</p>
-              I'm a passionate web developer, and this is where I share my work 
-              and ideas. Feel free to explore and get in touch if you’d like to 
-              collaborate!
-            </h5>
+      <div className="row g-0">
+          <div className="col-md-12 text-center">
+            <img className='img-fluid' src={Vincent} style={{ width: '35rem' }} />
           </div>
-          <div className="col-md-4"></div>
-        </div>
+          
+          <div className="row g-0 ">
+              <div className="col-md-3"></div>
+                <div className="col-md-6 text-center">
+                  <h5 className='text-white p-4 greeting rounded' >
+                    <p className='welcome'>Welcome to My Corner of the Web!</p>
+                    I'm a passionate web developer, and this is where I share my work 
+                    and ideas. Feel free to explore and get in touch if you’d like to 
+                    collaborate!
+                    <br></br>
+                    <button className='mt-3 btn btn-lg btn-warning resume'>Download CV</button>
+                  </h5>
+                  
+                </div>
+              <div className="col-md-3"></div>
+          </div>
       </div>
 
     </div>
